f65665045e730a50da93a8dbe774f63ab425f0f1
Ryan Christiani <ryan@ryanchristiani.com>
2014-05-01 22:15:34 -0400

    Playing around trying to figure out how to run banner from node

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var argv = require('optimist').argv;
 var http = require('http');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
 
 var apiKey = 'a30713ae62fb4d00';
 var location;
@@ -12,19 +15,28 @@ if(argv.location !== undefined || argv.loc !== undefined) {
 	var country = splitList[0];
 	var city = splitList[1];
 	var apiUrl = 'http://api.wunderground.com/';
-	var path = 'api/a30713ae62fb4d00/geolookup/conditions/forecast/q/'+ country +'/' + city + '.json';
+	var path = 'api/a30713ae62fb4d00/geolookup/forecast/q/'+ country +'/' + city + '.json';
 	var data = '';
 	http.get(apiUrl + path, function(res) {
 		res.on('data',function(chunk) {
 			data += chunk;
 		});
 		res.on('end', function() {
-			console.log(data);
+			// Comes back as string, so you need to parse it
+			data = JSON.parse(data);
+			banner(data);
 		});
 	});	
 }
 else {
 	console.log('Please provide a location with either --location or --loc');
 }
+
+function banner(data) {
+	exec('banner', function(error, stdout, stdin) {
+		sys.puts(stdout);
+	});
+}
+
 
 console.log(location);
