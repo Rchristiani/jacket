7d0e0ca856e73472d25bbef08bb52b17d41ce807
Ryan Christiani <ryan@ryanchristiani.com>
2014-05-01 21:57:56 -0400

    Working out getting data and collection info

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
 
 var argv = require('optimist').argv;
+var http = require('http');
 
 var apiKey = 'a30713ae62fb4d00';
 var location;
 
 if(argv.location !== undefined || argv.loc !== undefined) {
 	location = argv.location || argv.loc;
+	var splitList = location.split(',');
+	var country = splitList[0];
+	var city = splitList[1];
+	var apiUrl = 'http://api.wunderground.com/';
+	var path = 'api/a30713ae62fb4d00/geolookup/conditions/forecast/q/'+ country +'/' + city + '.json';
+	var data = '';
+	http.get(apiUrl + path, function(res) {
+		res.on('data',function(chunk) {
+			data += chunk;
+		});
+		res.on('end', function() {
+			console.log(data);
+		});
+	});	
 }
-
-if(argv.location === undefined) {
-	console.log("Please set a location");
-	return;
+else {
+	console.log('Please provide a location with either --location or --loc');
 }
 
 console.log(location);
